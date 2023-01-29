0248f79c2e07064b854d2257442cd9aa8b84cfff
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-01-29 08:54:15 -0800

    Add `DrumPads` component
    - addresses #3

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('Renders outer container', () => {
-  render(<App />)
-  const outerContainer = screen.getByTestId('drum-machine')
-  expect(outerContainer).toBeInTheDocument()
-})
+describe('Drum Machine', () => {
+  test('renders outer container', () => {
+    render(<App />)
+    const outerContainer = screen.getByTestId('drum-machine')
+    expect(outerContainer).toBeInTheDocument()
+  })
 
-test('renders display container', () => {
-  render(<App />)
-  const displayContainer = screen.getByTestId('display')
-  expect(displayContainer).toBeInTheDocument()
+  test('renders display container', () => {
+    render(<App />)
+    const displayContainer = screen.getByTestId('display')
+    expect(displayContainer).toBeInTheDocument()
+  })
+
+  test('renders drum pad container', () => {
+    render(<App />)
+    const drumPadContainer = screen.getByTestId('drum-pads')
+    expect(drumPadContainer).toBeInTheDocument()
+  })
 })
