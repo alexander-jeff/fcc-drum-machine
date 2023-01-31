6e18fdfc306e17c4fd5d09cdfa3449c29835f8a8
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-01-31 08:08:57 -0800

    Resolve #6 - Add keyPress listener on mount
    - remove listener on unmount

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
 import './DrumPads.css'
 
 const DrumPad = ({ drum, handleClick }) => {
   const { id, letter, src } = drum
+  useEffect(() => {
+    const onKeyPress = ({ key }) => {
+      key.toUpperCase() === letter && handleClick(letter)
+    }
+
+    // on mount
+    document.addEventListener('keydown', onKeyPress)
+
+    // on unmount
+    return () => document.removeEventListener('keydown', onKeyPress)
+  }, [handleClick, letter])
   return (
     <div
       className='drum-pad'
